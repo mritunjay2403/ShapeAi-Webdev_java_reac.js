37c22b7ca4694dd42b78641500d076fc2ee64b8e
mritunjay2403 <81086938+mritunjay2403@users.noreply.github.com>
2021-06-25 03:31:49 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        {" "}
+        <p>Copyright by shapeAI @ {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
